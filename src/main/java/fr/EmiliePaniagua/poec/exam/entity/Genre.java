043416b24c350228ffe87c86bdba6c2e03648ad7
Emilie <emilie.paniagua@gmail.com>
2024-01-22 12:56:34 +0100

    Entities without connections

--- src/main/java/fr/EmiliePaniagua/poec/exam/entity/Genre.java
+++ src/main/java/fr/EmiliePaniagua/poec/exam/entity/Genre.java
@@ -1,4 +1,8 @@
 package fr.EmiliePaniagua.poec.exam.entity;
 
 public class Genre {
+
+    private Long id;
+
+    private String name;
 }
--- src/main/java/fr/EmiliePaniagua/poec/exam/entity/Review.java
+++ src/main/java/fr/EmiliePaniagua/poec/exam/entity/Review.java
@@ -1,4 +1,31 @@
 package fr.EmiliePaniagua.poec.exam.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
 public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    private LocalDateTime createdAt;
+
+    private float note;
+
+    private LocalDateTime ModerationDate;
 }
